4997d2477cba67b8ddc2c1b51b3770e9151b9ace
Chanjin Baek <78646691+Chanjinee@users.noreply.github.com>
2023-12-11 22:30:48 +0900

    Update dags_python_show_templates.py

--- dags/dags_python_show_templates.py
+++ dags/dags_python_show_templates.py
@@ -6,7 +6,7 @@ from airflow.decorators import task
 with DAG(
     dag_id="dags_python_show_templates",
     schedule="30 9 * * *",
-    start_date=pendulum.datetime(2023, 3, 10, tz="Asia/Seoul"), # 해당 강의 찍는 당시 날짜 3/29
+    start_date=pendulum.datetime(2023, 12, 1, tz="Asia/Seoul"), # 해당 강의 찍는 당시 날짜 3/29
     catchup=True # 3/10일부터 3/29까지 모든 일정 코드 돌림
 ) as dag:
     
