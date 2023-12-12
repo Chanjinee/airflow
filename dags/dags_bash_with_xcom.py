f6d4c6a4e9892d8c9d0588e28841455ad672fe86
Chanjin Baek <78646691+Chanjinee@users.noreply.github.com>
2023-12-12 22:21:58 +0900

    bashOperator에서 xcom 사용

---
+++ dags/dags_bash_with_xcom.py
@@ -0,0 +1,28 @@
+from airflow import DAG
+import pendulum
+import datetime
+from airflow.operators.bash import BashOperator
+
+with DAG(
+    dag_id="dags_bash_with_xcom",
+    schedule="10 0 * * *",
+    start_date=pendulum.datetime(2023, 3, 1, tz="Asia/Seoul"),
+    catchup=False
+) as dag: # BashOperator에서 xcom을 사용하기 위해서는 템플릿 언어를 사용해야함
+    bash_push = BashOperator(
+    task_id='bash_push',
+    bash_command="echo START && " # echo 하는 것도 return이랑 동일하게 취급됨
+                 "echo XCOM_PUSHED "
+                 "{{ ti.xcom_push(key='bash_pushed',value='first_bash_message') }} && "
+                 "echo COMPLETE"
+    )
+
+    bash_pull = BashOperator(
+        task_id='bash_pull',
+        env={'PUSHED_VALUE':"{{ ti.xcom_pull(key='bash_pushed') }}",
+            'RETURN_VALUE':"{{ ti.xcom_pull(task_ids='bash_push') }}"},
+        bash_command="echo $PUSHED_VALUE && echo $RETURN_VALUE ",
+        do_xcom_push=False
+    )
+
+    bash_push >> bash_pull
