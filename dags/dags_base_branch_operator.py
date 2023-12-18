094208cfdcf84846ee012fc2eb3356e6ceba470c
Chanjin Baek <78646691+Chanjinee@users.noreply.github.com>
2023-12-18 23:19:18 +0900

    Update dags_base_branch_operator.py

--- dags/dags_base_branch_operator.py
+++ dags/dags_base_branch_operator.py
@@ -12,7 +12,7 @@ with DAG(
     class CustomBranchOperator(BaseBranchOperator): # 공식 가이드를 보면, 무조건 해당 클래스를 상속해야함
         def choose_branch(self, context): # 무조건 이 함수를 만들고 오버라이딩 하도록 가이드에 나옴 (이름도 바꾸면 안됨, 안에 들어가는 인자도 변경 X)
             import random
-            print(context) #
+            print(context) # 파라미터들이 들어있음(data_interval_start, 등 시간, 등에 관련된 파라미터 -> 이후 꺼내서 사용하면 됨)
             
             item_lst = ['A', 'B', 'C']
             selected_item = random.choice(item_lst)
